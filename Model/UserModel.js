c22328f09dd777ff672b92f37df63e69b3748842
wtyo1768 <wtyo1768@gmail.com>
2019-06-22 23:50:00 +0800

    merge

--- Model/UserModel.js
+++ Model/UserModel.js
@@ -14,7 +14,7 @@ var UserSchema = new mongoose.Schema({
 
     username: { type: String, default: "郭家銘" },
     nickname: { type: String, default: this.username },
-    coin: { type: Number, default: 3000 },
+    coin: { type: Number, default: 6000 },
     coinMax : { type : Number , default : 5000},
     
     diamond: { type: Number, default: 0 },
