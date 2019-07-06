7d6e595f8a17460b9ad3b4d5a11a0f3ad5a21db7
wtyo1768 <wtyo1768@gmail.com>
2019-07-06 19:59:09 +0800

    webhook

--- webhook/api.js
+++ webhook/api.js
@@ -1,18 +1,38 @@
 const app = require('express')();
 const PORT = process.env.port || 3000;
+const { exec } = require('child_process');
 
 app.listen(PORT, () => {
     console.log(`webhook api listen on ${PORT}`);
 })
 
-
+const cd = 'cd ~/../home/wtyo1768';
 const cmd = {
-    pull : 'git pull origin',
-    build : 'npm run build',
+    pull: `${cd}/kyronus && git pull origin`,
+    rebuild: `${cd}/kyronus  && npm run build`,
+    nginx_rebuild: `${cd}/`
 }
 
 
-app.get('/webhook',(req,res)=>{
+app.get('/webhook', (req, res) => {
     console.log('WebHookk')
     console.log(req)
+    exec(cmd.pull, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err)
+            res.status(500).end();
+        }
+        else {
+            console.log(stdout)
+            exec(cmd.rebuild, (err, stdout, stderr) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).end();
+                }
+                else {
+                    console.log(stdout)
+                }
+            })
+        }
+    })
 })
