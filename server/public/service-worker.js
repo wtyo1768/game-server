4566376ce2003a636402e6b8a5e243f036ea3f48
ishtien <203isthebest@gmail.com>
2019-07-10 12:38:30 +0800

    Try serviceworker0.4 err

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,29 +1,29 @@
-// /**
-//  * Welcome to your Workbox-powered service worker!
-//  *
-//  * You'll need to register this file in your web app and you should
-//  * disable HTTP caching for this file too.
-//  * See https://goo.gl/nhQhGp
-//  *
-//  * The rest of the code is auto-generated. Please don't update this file
-//  * directly; instead, make changes to your Workbox build configuration
-//  * and re-run your build process.
-//  * See https://goo.gl/2aRDsh
-//  */
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-// importScripts(
-//   "/precache-manifest.c568139fed73648cfad18a09dde4050c.js"
-// );
+importScripts(
+  "/precache-manifest.9e87e5afdf64ebaa3a1b99d9edc3ed6b.js"
+);
 
-// workbox.core.setCacheNameDetails({prefix: "kyronus"});
+workbox.core.setCacheNameDetails({prefix: "kyronus"});
 
-// /**
-//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-//  * requests for URLs in the manifest.
-//  * See https://goo.gl/S9QRab
-//  */
-// self.__precacheManifest = [].concat(self.__precacheManifest || []);
-// workbox.precaching.suppressWarnings();
-// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
