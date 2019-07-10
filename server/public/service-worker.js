3daf740f083890b5d0280a803ba05b5f4cb8c7ef
ishtien <203isthebest@gmail.com>
2019-07-10 22:39:59 +0800

    Deploy

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.749ee7d47165c0d5f167900ec5ee7344.js"
+  "/precache-manifest.5196bf06cf43fe9b355440deda803d51.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kyronus"});
