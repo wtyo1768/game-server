a66dcd59360769d35b9e94036b2711830f4eec04
ishtien <203isthebest@gmail.com>
2019-07-10 16:42:37 +0800

    try serviceworker1.0

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,29 +1,11 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.67fe3f2b260a91cb5ed491a8131101aa.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+if (workbox) {
+  console.log(`Workbox is loaded`);
 
-importScripts(
-  "/precache-manifest.86d1d3b4d75bf9b834ce886750f7e4ff.js"
-);
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-workbox.core.setCacheNameDetails({prefix: "kyronus"});
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+} 
+else {
+  console.log(`Workbox didn't load`);
+}
