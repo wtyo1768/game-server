37690625a6da24ef248afff78d9870e4a9b6501c
ishtien <203isthebest@gmail.com>
2019-07-10 12:12:39 +0800

    Try serviceworker0.2

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6c6d1054c32d2fcf472a7308e314954.js"
+  "/precache-manifest.c568139fed73648cfad18a09dde4050c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kyronus"});
