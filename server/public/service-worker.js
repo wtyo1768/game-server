97663a2e528a4cd9fa8f83c545eb5dd195676a3c
ishtien <203isthebest@gmail.com>
2019-07-11 17:39:58 +0800

    Copy AInimal code

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -11,22 +11,53 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.8ab7533fecdf9e67f481971325ab978d.js"
-);
+  'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
+)
 
-workbox.core.setCacheNameDetails({prefix: "kyronus"});
+importScripts('/precache-manifest.8ab7533fecdf9e67f481971325ab978d.js')
 
-workbox.skipWaiting();
-workbox.clientsClaim();
+workbox.core.setCacheNameDetails({ prefix: 'kyronus' })
+
+workbox.clientsClaim()
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+self.addEventListener('install', function(event) {
+  console.log('[PWA] Installing service worker')
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      console.log('cache', cacheNames)
+      self.clients.claim()
+    })
+  )
+})
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
