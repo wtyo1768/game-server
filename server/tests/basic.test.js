5bfd61df24b5ef04d8cd21b2ae04895f0d261d65
Azoo <azoolele@hmail.com>
2019-07-12 21:37:31 +0800

    update test file

--- server/tests/basic.test.js
+++ server/tests/basic.test.js
@@ -4,11 +4,26 @@ var request = require('request').defaults({ jar: true });
 
 const base = 'http://localhost:3000';
 const headers = { 'Content-Type': 'text/plain' };
-const form = {
-    "email": "test@yahoo.com.tw",
-    "username": "yosa",
-    "password": "123456"
-};
+// const form = {
+//     "email": "test@yahoo.com.",
+//     "username": "yosa",
+//     "password": "123456"
+// };
+const form = [
+    {
+        "email": "test@yahoo.com.",
+        "username": "yosa",
+        "password": "123456"
+    }, {
+        "email": "azookeke@gmail.com",
+        "username": "hahaha",
+        "password": "111111"
+    }, {
+        "email": "dosent_exist@gmail.com",
+        "username": "!@#$%",
+        "password": "111111"
+    }
+] 
 
 describe('Server Init', () => {
     it('should return 200', done => {
@@ -19,13 +34,27 @@ describe('Server Init', () => {
     })
 })
 describe('User', () => {
-    describe('#Register-normal', () => {
-        it('should return [201,400,406]', done => {
+    describe('#Register', () => {
+        it('should return 201,400,406', done => {
             request.post({ url: base + '/user/register', headers }, (err, res, body) => {
                 expect(res.statusCode).to.be.oneOf([201, 400, 406])
                 console.log(res.statusCode)
                 done();
-            }).form(form);
+            }).form(form[0]);
+        })
+        it('should return 400', done => {
+            request.post({ url: base + '/user/register', headers }, (err, res, body) => {
+                expect(res.statusCode).to.equal(400)
+                console.log(res.statusCode)
+                done();
+            }).form(form[2]);
+        })
+        it('should return 406', done => {
+            request.post({ url: base + '/user/register', headers }, (err, res, body) => {
+                expect(res.statusCode).to.equal(406)
+                console.log(res.statusCode)
+                done();
+            }).form(form[1]);
         })
     })
     describe('#login', () => {
@@ -34,7 +63,21 @@ describe('User', () => {
                 expect(res.statusCode).to.be.oneOf([401 ,404 , 200])
                 console.log(res.statusCode)
                 done();
-            }).form(form);
+            }).form(form[0]);
+        })
+        it('should return 401', done => {
+            request.post(base+'/user/login',(err,res,body)=>{
+                expect(res.statusCode).to.equal(401)
+                console.log(res.statusCode)
+                done();
+            }).form(form[1]);
+        })
+        it('should return 404', done => {
+            request.post(base+'/user/login',(err,res,body)=>{
+                expect(res.statusCode).to.equal(404)
+                console.log(res.statusCode)
+                done();
+            }).form(form[2]);
         })
     })
 });
