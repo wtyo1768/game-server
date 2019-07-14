3c8279b86baa55e03331840d1595456ea9885484
ishtien <203isthebest@gmail.com>
2019-07-15 01:01:23 +0800

    Change serviceworker customize

---
+++ server/public/customServiceWorker.js
@@ -0,0 +1,47 @@
+importScripts("/precache-manifest.a21ef7ec65de2270285c9fb929703504.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.core.setCacheNameDetails({ prefix: 'kyronus' })
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+self.addEventListener('install', function(event) {
+  console.log('[PWA] Installing service worker')
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        console.log('cache', cacheNames)
+        return Promise.all(
+          cacheNames.map(cacheToDelete => {
+            return caches.delete(cacheToDelete)
+          })
+        )
+      })
+      .then(() => self.clients.claim())
+  )
+})
+
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
