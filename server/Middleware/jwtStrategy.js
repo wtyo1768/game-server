6a136438e506abf318c169699b4844ad500a6994
wtyo1768 <wtyo1768@gmail.com>
2019-07-15 19:40:05 +0800

    add comment

--- server/Middleware/jwtStrategy.js
+++ server/Middleware/jwtStrategy.js
@@ -3,7 +3,6 @@ const UserModel = require('../Model/UserModel');
 
 const extrctJwtFromCookie = function (req) {
     let token =  req.cookies.auth
-    // console.log(token)
     return token;
 }
 
@@ -17,9 +16,10 @@ const strategy = new jwtStrategy(opts, function (payload, done) {
         if (err)
             return done(err, false, 500);
         if (user){
+            // - Convert Mongoose Document type to js object
+            // - So we can delete the user's secret info 
             user = user.toObject();
             user.password = user.__v = undefined;
-            console.log('jwt')
             return done(null, user);
         }
         else
