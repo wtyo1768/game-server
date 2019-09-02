379834dec3261c833b53fc275dbc779f43786954
wtyo1768 <wtyo1768@gmail.com>
2019-09-02 22:49:31 +0800

    Oauth callback url

--- server/config/config.js
+++ server/config/config.js
@@ -5,13 +5,15 @@ const config = {
   'google': {
     'clientID': '756112742682-h73nrgl0lo5e22uf8t3hrhajhd0hav9i.apps.googleusercontent.com',
     'clientSecret': 'KEFFfkx5AJHYHslH3YcCPy8g',
-    'callbackURL': 'https://test.citychaser.com/api/user/Auth',
+    'callbackURL': (process.env.NODE_ENV.trim() == "development") ?
+     'http://localhost:3000/api/user/Auth' : 'https://test.citychaser.com/api/user/Auth',
   },
   
   'facebook' : { 
     'ID' : '2218872001499698',
     'secret' :'f259a5f277d9dc71928d7c2a897772f2',
-    'callbackURL': 'https://test.citychaser.com/api/user/facebook/cb',
+    'callbackURL': (process.env.NODE_ENV.trim() == "development") ?
+    'http://localhost:3000/api/user/facebook/cb' :'https://test.citychaser.com/api/user/facebook/cb',
   },
 }
 
