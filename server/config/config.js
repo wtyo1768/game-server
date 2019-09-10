64af19b544dc03a34dd9a82ebe48422dc2fe2e8b
wtyo1768 <wtyo1768@gmail.com>
2019-09-11 00:03:52 +0800

    revise third party login callback url

--- server/config/config.js
+++ server/config/config.js
@@ -6,14 +6,14 @@ const config = {
     'clientID': '756112742682-h73nrgl0lo5e22uf8t3hrhajhd0hav9i.apps.googleusercontent.com',
     'clientSecret': 'KEFFfkx5AJHYHslH3YcCPy8g',
     'callbackURL': (process.env.NODE_ENV.trim() == "development") ?
-     'http://localhost:3000/api/user/Auth' : 'https://test.citychaser.com/api/user/Auth',
+     'http://localhost:3000/api/user/Auth' : 'https://test.citychaserstudio.com/api/user/Auth',
   },
   
   'facebook' : { 
     'ID' : '2218872001499698',
     'secret' :'f259a5f277d9dc71928d7c2a897772f2',
     'callbackURL': (process.env.NODE_ENV.trim() == "development") ?
-    'http://localhost:3000/api/user/facebook/cb' :'https://test.citychaser.com/api/user/facebook/cb',
+    'http://localhost:3000/api/user/facebook/cb' :'https://test.citychaserstudio.com/api/user/facebook/cb',
   },
 }
 
